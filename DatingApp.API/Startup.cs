2278ff4c860615ba6245a7cdee77b132a88b5797
Rand Jackson <randjackson3@gmail.com>
2020-06-28 22:09:19 -0400

    Front end has access to DB

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))); // registers context as a service in the IServiceCollection
             services.AddControllers();
+            services.AddCors(); // enable cross origin resource sharing 
         } // called by ASP.NET Core runtime when the app starts
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,8 @@ namespace DatingApp.API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); // allow any sight to make cross-origin requests, with any method or headers
+             
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
