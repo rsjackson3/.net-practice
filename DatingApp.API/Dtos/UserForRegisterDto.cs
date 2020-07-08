4eff8931e3bab0da8be7162454b206c5a081f5dd
Rand Jackson <randjackson3@gmail.com>
2020-07-07 21:20:02 -0400

    Created login method with JWT

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     // a DTO is used to collect data to be returned to the client from the backend 
     public class UserForRegisterDto
     {
+        [Required]
         public string Username {get; set;}
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 8 characters")]
         public string Password {get; set;}
     }
 }
