e029564890701c5ef63c7e81cee65574af01b796
Rand Jackson <randjackson3@gmail.com>
2020-07-30 20:39:55 -0400

    Added DB seed data

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -7,11 +7,33 @@ namespace DatingApp.API.Data
 {
     public class Seed
     {
+        // method will serialize JSON to user objects
         public static void SeedUsers(DataContext context) {
-            if (!context.Users.Any())
+            if (!context.Users.Any()) // make sure no users in DB
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                foreach (var user in users) {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+                    context.Users.Add(user);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())  // constructor randomly generates key, we will use as a salt
+            {
+                passwordSalt = hmac.Key; 
+                // must convert password to byte array for parameter of ComputeHash function
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)); 
+                
             }
         }
     }
